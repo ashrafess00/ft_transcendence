6c76160c2acca6006959864ac782326345af556b
Achraf Essaoudi <aessaoud@c2r5p1.1337.ma>
2024-01-16 10:17:38 +0100

    backend django added

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = async() => {
+    const routes = [
+        {path: "/"},
+        {path: "/posts"},
+        {path:"/posts/:id"},
+        {path:"/settings"}
+    ]
+}
