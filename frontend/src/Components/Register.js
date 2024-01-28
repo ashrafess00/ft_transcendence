a9154da0638dadf2d51a30ce9e053bbe654865d0
doffa-D <hossam.dagdag@gmail.com>
2024-01-28 11:05:55 +0100

    complite register page

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,16 +1,17 @@
-import BackToHome from "./BackToHome.js"
+import BackToHome from "./BackToHome.js";
 
 export default class Register {
     constructor() {
-        this.url = "http://localhost:8000/api/register/"
+        this.url = "http://localhost:8000/api/register/";
         this.username = "";
         this.email = "";
         this.password = "";
-        this.mssg = "";
+        this.confirm_password = "";  
+        this.message = "";
     }
 
     async render() {
-        let backToHomeCom = new BackToHome()
+        let backToHomeCom = new BackToHome();
 
         const html = `
         ${backToHomeCom.getHtml()}
@@ -23,57 +24,70 @@ export default class Register {
                     <div class="vr d-none d-lg-block"></div>
                     <form class="container-fluid" action="http://localhost:8000/api/register" method="POST" id="register">
                         <div class="mb-3">
-                            <label for="username" class="form-label">username</label>
-                            <input type="text" class="form-control" id="username" name="username">
-                            <div id="usernameHelp" class="form-text">something here</div>
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" class="form-control" id="username" name="username" required>
+                            <div id="usernameHelp" class="form-text">Enter your username</div>
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email">
-                            <div id="emailHelp" class="form-text">don't share your email a bb</div>
+                            <input type="email" class="form-control" id="email" required>
+                            <div id="emailHelp" class="form-text">Enter your email</div>
                         </div>
-                        <div class="mb-3"> 
+                        <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password">
-                            <div id="passwordHelp" class="form-text">don't share your password</div>
+                            <input type="password" class="form-control" id="password" required>
+                            <div id="passwordHelp" class="form-text">Enter your password</div>
                         </div>
-                        <div class="mb-3"> 
+                        <div class="mb-3">
                             <label for="confirm_password" class="form-label">Confirm Password</label>
-                            <input type="password" class="form-control" id="confirm_password">
-                            <div id="confirm_passwordHelp" class="form-text">don't share your password</div>
+                            <input type="password" class="form-control" id="confirm_password" required>
+                            <div id="confirm_passwordHelp" class="form-text">Confirm your password</div>
                         </div>
-                        <button type="submit" class="btn btn-primary text-white">Login</button>
+                        <div id="messageContainer"></div>
+
+                        <button type="submit" class="btn btn-primary text-white">Register</button>
                     </form>
                 </div>
             </div>
         </div>
-        `
+        `;
+
         document.querySelector("#app").innerHTML = html;
         const form = document.getElementById("register");
-        form.addEventListener("submit", e => {
+        form.addEventListener("submit", async e => {
             e.preventDefault();
-            let username = document.getElementById("username").value;
-            let email = document.getElementById("email").value;
-            let password = document.getElementById("password").value;
-            let confirm_password = document.getElementById("confirm_password").value;
-            console.log(username, email, password)
-            fetch(this.url, {
+            this.username = document.getElementById("username").value;
+            this.email = document.getElementById("email").value;
+            this.password = document.getElementById("password").value;
+            this.confirm_password = document.getElementById("confirm_password").value;
+            
+            const response = await fetch(this.url, {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
+                headers:{
+                    "Content-Type":"application/json",
                 },
-                body: JSON.stringify({
-                    username: username,
-                    password: password,
-                    email: email,
-                    confirm_password: confirm_password,
+                body:JSON.stringify({
+                    username: this.username,
+                    email: this.email,
+                    password: this.password,
+                    confirm_password: this.confirm_password,
                 }),
-            }).then(
-                res => {
-                    console.log("==> " ,res)
-                }
-            )
-        })
+            });
+            const data = await response.json();
+            if (!response.ok)
+            {
+                console.log(data.error)
+                this.message = data.error;
+            }
+            else
+            {
+                console.log(data.status)
+                this.message = data.status;
+            }
+
+            messageContainer.innerHTML = `<p>${this.message}</p>`;
+
+        });
 
     }
 }
